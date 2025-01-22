aa83c4463da57ad8e238f56915b958526eca2b80
Leen Aboukhalil <109052421+Leen-ak@users.noreply.github.com>
2025-01-22 14:27:42 -0500

    Adding a search bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
  import MovieCard from "../components/movieCard";
- 
+ import {useState} from "react"
+
  function Home(){
+    //0 = the name of the state 1 = the function that i will write, 
+    //and after the =  it is the default value of the state
+    const [searchQuery, setSearchQuery] = useState("");
+    
     const Movies = [
         {id: 1, title: "Oppenheimer", release_date: "2023"},
         {id: 2, title: "Avatar", release_date: "2022"},
@@ -9,18 +14,27 @@
         {id: 5, title: "Extraction", release_date: "2020"},
     ];
 
-    const handleSearch = () => {
+    const handleSearch = (e) => {
+        e.preventDefault();
 
     }
 
     return (
         <div className="home">
             <form OnSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for movies..." className="search-input"></input>
+                <input 
+                    type="text" 
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                    value={searchQuery} 
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                > 
+                </input>
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className="movie-grid">
                 { Movies.map((movie) => (
+                    movie.title.toLowerCase().startsWith(searchQuery) &&
                     < MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
